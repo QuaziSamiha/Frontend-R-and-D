7c1c5fe3b4a8709533dd4d38b6e56ac8fb4d3227
QuaziSamiha <1703155@student.ruet.ac.bd>
2024-12-20 20:46:01 +0600

    form build and validate manually

---
+++ src/app/form/simple-forms/page.tsx
@@ -0,0 +1,13 @@
+import SimpleForms from "@/components/form/simple-forms/SimpleForms";
+
+const Page = () => {
+  return (
+    <section className="flex justify-center items-center bg-stone-50 ">
+      <main className="container max-w-7xl py-20">
+        <SimpleForms />
+      </main>
+    </section>
+  );
+};
+
+export default Page;
