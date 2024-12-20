6cf0065ee5fc74b7952dc682e233e4f6f12b4f7f
QuaziSamiha <1703155@student.ruet.ac.bd>
2024-12-20 20:50:14 +0600

    form build and validate manually

--- src/components/form/simple-forms/SimpleForms.tsx
+++ src/components/form/simple-forms/SimpleForms.tsx
@@ -1,9 +1,16 @@
+import Link from "next/link";
 import Form1 from "./form1/Form1";
 
 const SimpleForms = () => {
   return (
     <>
       <section className="flex flex-col gap-16 w-full">
+        <Link
+          href={"/"}
+          className="bg-sky-700 px-6 py-2 rounded-md text-white w-fit"
+        >
+          Back to Home
+        </Link>
         <p className="text-2xl font-bold text-slate-700 text-center">
           Simple Forms
         </p>
